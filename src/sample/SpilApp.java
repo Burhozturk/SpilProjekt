4f63a20a6ae46987fbd469c335d5b0425484e9e6
Burhan Öztürk <burh0125@edu.easj.dk>
2019-01-05 17:42:51 +0100

    Tilføjede to Metoder med override. Den ene gad ikke virke med override

--- src/sample/SpilApp.java
+++ src/sample/SpilApp.java
@@ -10,12 +10,35 @@ public class SpilApp extends GameApplication
 {
 
     //Privat enum som deklareres.
-    private enum Type;
+    //private enum type;
 
+    //Override a method,
     @Override
+    //Protected, som er en klasse type, der kan ses fra subklasser og pakke medlemmer
+    //Denne er til initiasering af spillets grundværdier, der blandt er længde bredde og lignende detaljer
+    //Denne metode er med en parameter, som er en objekt af en klasse.
     protected void initSettings(GameSettings settings)
     {
+        //Objektet har en metode med en parameter(argumemt)
         settings.setWidth(1280);
+        settings.setHeight(600);
+        settings.setVersion("Version 1.0");
+        settings.setTitle("Spil app");
+        settings.setMenuEnabled(false);
+        settings.setIntroEnabled(true);
+
+
+    }
+
+    @Override
+    protected void initInput()
+    {
+
+    }
+
+    //@Override
+    protected void initAssets() throws Exception
+    {
 
     }
 
