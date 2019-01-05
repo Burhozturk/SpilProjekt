83bfe653d38c29cd00f3346425ff782873cfda41
Burhan Öztürk <burh0125@edu.easj.dk>
2019-01-05 17:25:01 +0100

    Tilføjede en klasse med nedarvning. Derudover har jeg kommenteret koden.

--- src/sample/SpilApp.java
+++ src/sample/SpilApp.java
@@ -1,11 +1,23 @@
 package sample;
 
+//Import fra forskellige klasser. Det meste er fra FXGL
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
 
+
+//Public klasser med nedarvning
 public class SpilApp extends GameApplication
 {
 
-    private enum type
+    //Privat enum som deklareres.
+    private enum Type;
+
+    @Override
+    protected void initSettings(GameSettings settings)
+    {
+        settings.setWidth(1280);
+
+    }
 
 
 }
